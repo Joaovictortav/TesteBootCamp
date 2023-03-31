378965ceec97b4c3dbcf1ada7a98823460f650c9
João Victor Tavares <joao.santos@cmtecnologia.com.br>
2023-03-31 12:01:47 -0300

    up

--- Kernel/Controllers/BuscaController.cs
+++ Kernel/Controllers/BuscaController.cs
@@ -6,10 +6,20 @@ public class BuscaController
 {
     public async Task<string> Get()
     {
+        var t = new RestClient("https://api.mercadolibre.com/", "GET");
+        var param = new Dictionary<string, object>();
+
+        param.TryAdd("q", "playstation");
+        param.TryAdd("status", "active");
+        
+        var result = await t.Run("sites/MLB/search", param);
+        return result;
+    }
+    public async Task<string> GetRap()
+    {
         var t = new RestClient("https://api.mercadolibre.com/sites/MLB/search?q=playstation&status=active", "GET");
         var param = new Dictionary<string, object>();
-        
-        
+
         var result = await t.Run("", param);
         return result;
     }
